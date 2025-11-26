3137c89d30dc8cb8304cf18dafd4177d9d9c317e
pulsepulse45 <pulsepulse45@gmail.com>
2025-11-26 11:11:39 -0500

    Updates

---
+++ db.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const mongoURI =  "mongodb://root:JcnkVe7xnutVd3B5k3J6C9no@172.21.239.155:27017";
+
+const connectToMongo = async (retryCount) => {
+    const MAX_RETRIES = 3;
+    const count = retryCount ?? 0;
+    try {
+        await mongoose.connect(mongoURI, { dbName: 'stayhealthybeta1'});
+        console.info('Connected to Mongo Successfully')
+
+        return;
+    } catch (error) {
+        console.error(error);
+        
+
+        const nextRetryCount = count + 1;
+
+        if (nextRetryCount >= MAX_RETRIES) {
+            throw new Error('Unable to connect to Mongo!');
+        }
+
+        console.info(`Retrying, retry count: ${nextRetryCount}`)
+
+        return await connectToMongo(nextRetryCount);
+
+    }
+};
+
+module.exports = connectToMongo;
